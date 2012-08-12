6176e5ac54cd253dd52d468803ccfce8841f85dc
Robin G. Aaberg <robin.garen@gmail.com>
2012-08-12 16:22:39 +0200

    Commented web-build.py

--- web-build.py
+++ web-build.py
@@ -2,16 +2,29 @@
 #coding: utf-8
 import sys, re, os, string, webbrowser, subprocess
 
+###		Web-build
+###		Current version rsyncs a given folder localy with a given folder
+###		On a remote host:  (LOCAL_DIR -->  REMOTE_HOST_URI).
+###				
+###		It does not care for the file being given to it on build.
+###
+###		When the sync is done, it opens a webbrowser to a given target url:
+###		(REMOTE_WEB_URI).
+###
+############################################################################
+
 REMOTE_HOST_URI = "technocake@marte.komsys.org:/var/www/boofrecords.no/"
 REMOTE_WEB_URI = "http://boof.technocake.net/"
 LOCAL_DIR = "/Users/technocake/boof/web/"
+
+#Empty Skeleton, don't mind this block.
 try:
 	pass
 except Exception as e:	print (""" Usage: web-build <file> """, e);	sys.exit(1)
 
-#Copy
+
+#Copy to remote host (Rsync)
 print subprocess.call("rsync -vaz %s %s" % (LOCAL_DIR,  REMOTE_HOST_URI), shell=True)
 
-print os.getcwd()
 #Browse result
 webbrowser.open(REMOTE_WEB_URI)
